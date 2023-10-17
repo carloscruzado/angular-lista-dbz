6413becd435e8be5819c38ed664d91b4c895e739
carloscruzado <carlos570570@gmail.com>
2023-10-16 19:13:04 -0500

    Fin de seccion4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+import { HeroModule } from './heroes/hero.module';
+
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/components/counter.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroModule
   ],
   providers: [],
   bootstrap: [AppComponent]
